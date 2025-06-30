2105ae4b72316e665ba290921983024a0e81936f
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2025-06-30 16:19:02 -0400

    Update page.tsx fix type error

--- wonder-cart/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
+++ wonder-cart/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -4,7 +4,7 @@ import { CategoryForm } from "./components/category-form";
 const CategoryPage = async ({
   params,
 }: {
-  params: { categoryId: string, storeId: String };
+  params: { categoryId: string, storeId: string };
 }) => {
   const category = await prismadb.category.findUnique({
     where: {
